8d28f1130682776c401796af48ce98ebf57db0be
wr3stl3r <61430801+wr3stl3r@users.noreply.github.com>
2020-09-09 18:31:53 +0300

    Update dart_loops.dart

--- dart_loops.dart
+++ dart_loops.dart
@@ -3,10 +3,13 @@ import 'dart:math';
 List<int> GL = [];
 var L = GL.length;
 var howBig =3;
+String S = "abcdefghijklmnoprstuvz";
+int RI = Random().nextInt(S.length);
+
 
 void mymom() {
   // makes list from i to 100*
-  for (int i = 0; i <= howBig *100; i = i + howBig) {
+  for (int i = 0; i <= howBig *10; i = i + howBig) {
     GL.add(i);
   }
 }
@@ -14,7 +17,10 @@ void mymom() {
 void main() {
   mymom();
   print("Hello the length of GL lit is ${L}");
+  print("I am trying to print letter from string: " + S[RI]);
+  //print(RI);
   for (int i = 0; i < L; i++) {
-    print('hello ${2 * i} slut ${GL[i]}\n'.toUpperCase()); //pow(2,i)
+    print('hello ${2 * i} slut ${GL[i]}, ${Random().nextInt(10000)}'.toUpperCase()); //pow(2,i)
+    //print(Random().nextInt(10000));
   }
 }
