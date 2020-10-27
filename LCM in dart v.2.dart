2cd96ab3951ab94e568330055efca628ea9067d3
wr3stl3r <61430801+wr3stl3r@users.noreply.github.com>
2020-10-28 01:45:22 +0200

    Create LCM in dart v.2.dart
    Added new function named "theEndList" which helps make this last necessary function much smaller so u can move other functions for LCM to another dart file. i used this in Flutter

---
+++ LCM in dart v.2.dart
@@ -0,0 +1,87 @@
+import 'dart:math';
+// finding LCM - "Least(smallest) common multiple" using Dart prog.lang
+// translated from https://github.com/wr3stl3r/edabit.com/blob/master/LCM%20from%20list.py
+main() {
+  print(lcm_of_list([22,55]));
+  print(lcm_of_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // 2520)
+  print(lcm_of_list([13, 6, 17, 18, 19, 20, 37])); // 27965340)
+  print(lcm_of_list([44, 64, 12, 17, 65])); // 2333760)
+  print(lcm_of_list([200, 30, 18, 11, 8, 64, 34])); // 2692800)
+} //main
+
+lcm_of_list(numbers) {
+  List L = theEndList(numbers);
+  int end_multiple = 1; // this will be end result
+  L.forEach((element) {
+    end_multiple *= element;
+  });
+  return end_multiple;
+} //--LCM of list
+
+//----------------------------------------
+theEndList(List X){
+    //print(numbers);
+  List BIGL = [];
+  Set MEGA = new Set();
+  var res = [];
+  int C = 0;
+  // print();
+  X.forEach((e) => C += 1);
+  for (int i = 0; i < C; i++) {
+    //print("${C}<><><${i}---${numbers[i]}");
+    BIGL.add(primefactors(X[i]));
+  }
+  BIGL.forEach((subL) {
+    subL.forEach((elem) {
+      MEGA.add(elem);
+    });
+  });
+  //return MEGA;
+  //[2,2,2,3,3,3], [2,3,3]
+  MEGA.forEach((cipars) {
+    var tempo = [];
+    BIGL.forEach((subL) {
+      if (subL.isEmpty == false) {
+        tempo.add(count(subL, cipars));
+      }
+    });
+    //print(tempo);
+    tempo.sort();
+    res.add(pow(cipars,tempo.last));
+    tempo = [];
+  });
+  return res;
+  //var ALL_ONE = [MEGA,res, BIGL]; for testing
+}
+//-------------------------------------
+
+
+count(X, element) {
+  //counter
+  if (X.isEmpty || X == null) {
+    return 0;
+  }
+  int C = 0;
+  for (var i = 0; i < X.length; i++) {
+    if (X[i] == element) {
+      C++;
+    }
+  }
+  return C;
+} //counter
+
+//--------------------
+primefactors(X) {
+  int D = 2;
+  List<int> L = [];
+  while (X > 1) {
+    //print("${L},${D},X=${X}");
+    if (X % D == 0) {
+      L.add(D);
+      X = X / D;
+    } else {
+      D = D + 1;
+    } // else
+  } // while
+  return L; // return from primefactors()
+} //prime factors
